5f5f09ca8047c53ebae4ba8066d1232e7fbcdfa5
Murugaraj Rengaraj <murugarajr@gmail.com>
2023-11-09 23:26:08 +0000

    modified conftest

--- code_template_1/conftest.py
+++ code_template_1/conftest.py
@@ -4,10 +4,31 @@ from pytest import fixture
 
 @fixture
 def global_fixture():
+    """
+    Fixture for global setup.
+
+    This function is a fixture that is executed once before any tests in the test suite.
+    It performs global setup tasks, such as initializing resources or setting up the environment.
+
+    Parameters:
+        None
+
+    Returns:
+        None
+    """
     print("\n(Doing global fixture setup stuff!)")
 
 
 def pytest_configure(config):
+    """
+    Add markers to the pytest configuration.
+
+    Args:
+        config (object): The pytest configuration object.
+
+    Returns:
+        None
+    """
     config.addinivalue_line(
         "markers", "db: Example marker for tagging Database related tests"
     )
