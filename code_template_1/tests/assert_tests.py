8d5901dcaee6dee61daab59f138edb79eeabf5ad
Raj Rengaraj <murugarajr@gmail.com>
2026-04-01 15:36:24 +0100

    chore: apply Misoki safe fix to code_template_1/tests/assert_tests.py

--- code_template_1/tests/assert_tests.py
+++ code_template_1/tests/assert_tests.py
@@ -6,7 +6,7 @@ def test_div_zero_exception():
     pytest.raises can assert that exceptions are raised (catching them)
     """
     with pytest.raises(ZeroDivisionError):
-        x = 1 / 0
+        _ = 1 / 0
 
 
 def test_keyerror_details():
@@ -16,7 +16,7 @@ def test_keyerror_details():
     my_map = {"foo": "bar"}
 
     with pytest.raises(KeyError) as ke:
-        baz = my_map["baz"]
+        _ = my_map["baz"]
 
     # Our KeyError should reference the missing key, "baz"
     assert "baz" in str(ke)
